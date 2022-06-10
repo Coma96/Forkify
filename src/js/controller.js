f90c1fa80a476a67e19d03fe48ec61efec489001
TriSarme <vreco.milan@gmail.com>
2022-06-10 17:42:04 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -121,7 +121,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
-
+const newFeature = function () {
+  console.log('asdasd');
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -130,6 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
