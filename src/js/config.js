a291dbf13809a816bcd402e75ba7b6154cad6a1e
TriSarme <vreco.milan@gmail.com>
2022-06-10 17:31:58 +0200

    Initialc commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c036dd36-9501-4efa-8e74-017c782a34dd';
+export const MODAL_CLOSE_SEC = 2.5;
